9fa33c4eaa7503d32904fc7239aaabed02d06934
Ayush Shukla <shukla.ayush0203@gmail.com>
2021-12-19 11:20:48 +0530

    ok

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
     return (
-        <secition className="section error-page">
+        <section className="section error-page">
             <div className="error-container">
                 <h1>Oops! Its a dead end buddy.</h1>
                 <Link to="/" className="btn btn-primary">
                     Go back Home
                 </Link>
             </div>
-        </secition>
+        </section>
     );
 };
 
